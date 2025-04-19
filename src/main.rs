60db99d77e6acc055f274a4ec637420a30635465
gitsudhir <strr7452@gmail.com>
2025-04-19 22:54:02 +0530

    led blink

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
+use esp_idf_hal::gpio::{Gpio2, Output, PinDriver};
+use esp_idf_hal::prelude::*;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    // Initialize ESP-IDF system patches
     esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
+    // Initialize the logger
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("😁Hello, Sudhir ");
+    log::info!("Starting LED Blink Program!");
+
+    // Initialize the GPIO pin 2 as an output pin (for example, on the ESP32, GPIO2 is often connected to the onboard LED)
+    let mut led_pin = PinDriver::output(unsafe { Gpio2::new() }).unwrap();
+
+    // Main loop for blinking the LED
+    loop {
+        // Turn the LED on
+        led_pin.set_high().unwrap();
+        log::info!("LED ON");
+        thread::sleep(Duration::from_secs(1));
+
+        // Turn the LED off
+        led_pin.set_low().unwrap();
+        log::info!("LED OFF");
+        thread::sleep(Duration::from_secs(1));
+    }
 }
